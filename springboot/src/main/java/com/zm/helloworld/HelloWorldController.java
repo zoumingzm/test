d8863a8736a2f7828eec4b89cfa273705a7d6401
zouming <345434645@qq.com>
2017-03-06 23:31:10 +0800

    helloworld

---
+++ springboot/src/main/java/com/zm/helloworld/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.zm.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by Administrator on 2017/3/6.
+ */
+
+@RestController
+@EnableAutoConfiguration
+public class HelloWorldController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "hello world!";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(HelloWorldController.class,args);
+    }
+}
