13ea9c0be7e72dc7db27537c392462e9580aade5
ming.zou <ming.zou@dianrong.com>
2017-03-24 17:54:53 +0800

    简单json串解析

--- springboot/src/main/java/com/zm/Swagger2Config.java
+++ springboot/src/main/java/com/zm/Swagger2Config.java
@@ -2,6 +2,7 @@ package com.zm;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,11 +17,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@Profile("!dev")
 public class Swagger2Config {
 
     @Bean
     public Docket createRestApi(){
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.SWAGGER_2).groupName("Swagger2Config")
             .apiInfo(apiInfo())
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.zm"))
